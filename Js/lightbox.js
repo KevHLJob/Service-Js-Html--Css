6645eb8d924ab6707b5c38ec13e7a45cfba6c1a9
KevHLJob <KevHLJob@github.com>
2022-01-11 23:13:19 -0600

    first commit

---
+++ Js/lightbox.js
@@ -0,0 +1,17 @@
+const imagen = document.querySelector('.contenedor-galeria');
+const imageneslight = document.querySelector('.agregar-imagen');
+
+const contenedorlight = document.querySelector('.imagen-light');
+
+
+// console.log(imagen)
+//  console.log(imageneslight)
+//  console.log(contenedorlight)
+
+
+imagen.forEach(Element => {
+
+    imag.addEventListener('click', () => {
+        alert("papasitosoy")
+    })
+})
---
+++ Js/menu.js
@@ -0,0 +1,23 @@
+const opciones = document.querySelector('.opciones');
+
+const menu = document.querySelector('.menu-navegacion');
+
+// console.log(menu)
+// console.log(opciones)
+
+opciones.addEventListener('click', () => {
+
+    // alert("click")
+    menu.classList.toggle("spread")
+
+})
+
+window.addEventListener('click', e => {
+
+    if (menu.classList.contains('spread')
+        && e.target != menu && e.target != opciones) {
+        menu.classList.toggle("spread")
+    }
+
+
+})
